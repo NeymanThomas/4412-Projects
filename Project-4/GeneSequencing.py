031652371ccffde016adec491ec7c61594defbcb
ThomasNeyman <neymanthomas45@gmail.com>
2022-03-16 04:21:45 -0600

    Finished banded algorithm Implementation

--- Project-4/GeneSequencing.py
+++ Project-4/GeneSequencing.py
@@ -13,7 +13,7 @@ import math
 import time
 
 # Used to compute the bandwidth for banded version
-MAXINDELS = 3
+MAXINDELS = 3	# This is our d value
 
 # Used to implement Needleman-Wunsch scoring
 MATCH = -3
@@ -52,7 +52,7 @@ class GeneSequencing:
 					sequence_to_compute2 = sequence_to_compute2[:self.MaxCharactersToAlign]
 
 					if banded:
-						return
+						score, alignment1, alignment2 = self.banded_algorithm(sequence_to_compute1, sequence_to_compute2)
 					else:
 						score, alignment1, alignment2 = self.unrestricted_algorithm(sequence_to_compute1, sequence_to_compute2)
 
@@ -65,10 +65,11 @@ class GeneSequencing:
 		return results
 
 	# Unbanded algorithm implementation 
+	# https://wilkelab.org/classes/SDS348/2019_spring/labs/lab13-solution.html
 	def unrestricted_algorithm(self, seq1, seq2):
 		n = len(seq1)
 		m = len(seq2)
-		score = self.zeros(m + 1, n + 1)
+		score = self.init_matrix(m + 1, n + 1)
 		final_score = 0
 
 		# First fill out the first row and column
@@ -78,7 +79,7 @@ class GeneSequencing:
 		for i in range(0, n + 1):
 			score[0][i] = INDEL * i
 		
-		for i in range(1, m+1):
+		for i in range(1, m + 1):
 			for j in range(1, n + 1):
 				# Calculate the scores
 				match = score[i - 1][j - 1] + self.match_score(seq1[j - 1], seq2[i - 1])
@@ -130,9 +131,77 @@ class GeneSequencing:
 
 		return(final_score, align1, align2)
 
+	# Banded Algorithm implementation
 	def banded_algorithm(self, seq1, seq2):
-		return
-	
+		# Just tests to see if we are trying to align sequences that are too different in length
+		# Notice that the cutoff is a difference of 50, so if each sequence was only 20 characters
+		# long, it would not be stopped from aligning the test sequences with the genetic sequences
+		if len(seq1) - len(seq2) > 50 or len(seq2) - len(seq1) > 50:
+			return(math.inf, "No Alignment Possible", "No Alignment Possible")
+		
+		n = len(seq1)
+		m = len(seq2)
+		score = self.init_matrix(m + 1, n + 1)
+		final_score = 0
+
+		for i in range(0, m + 1):
+			score[i][0] = INDEL * i
+		
+		for i in range(0, n + 1):
+			score[0][i] = INDEL * i
+
+		for i in range(1, m + 1):
+			for j in range(1, n + 1):
+				if j - i <= MAXINDELS and i - j <= MAXINDELS:
+					match = score[i - 1][j - 1] + self.match_score(seq1[j - 1], seq2[i - 1])
+					delete = score[i - 1][j] + INDEL
+					insert = score[i][j - 1] + INDEL
+					score[i][j] = min(match, delete, insert)
+					final_score = score[i][j]
+
+		# Create variables to hold alignments
+		align1 = ""
+		align2 = ""
+		# Start at bottom right cell
+		i = m
+		j = n
+
+		while i > 0 and j > 0:
+			score_current = score[i][j]
+			score_diagonal = score[i - 1][j - 1]
+			score_up = score[i][j - 1]
+			score_left = score[i - 1][j]
+
+			if score_current == score_diagonal + self.match_score(seq1[j - 1], seq2[i - 1]):
+				align1 += seq1[j - 1]
+				align2 += seq2[i - 1]
+				i -= 1
+				j -= 1
+			elif score_current == score_up + INDEL:
+				align1 += seq1[j - 1]
+				align2 += '-'
+				j -= 1
+			elif score_current == score_left + INDEL:
+				align1 += '-'
+				align2 += seq2[i - 1]
+				i -= 1
+
+		while j > 0:
+			align1 += seq1[j - 1]
+			align2 += '-'
+			j -= 1
+		
+		while i > 0:
+			align1 += '-'
+			align2 += seq2[i - 1]
+			i -= 1
+		
+		# flip the alignment paths
+		align1 = align1[::-1]
+		align2 = align2[::-1]
+
+		return(final_score, align1, align2)
+
 	def match_score(self, a, b):
 		if a == b:
 			return MATCH
@@ -155,14 +224,27 @@ class GeneSequencing:
 				if j != len(mat[i]) - 1:
 					print("\t", end = "")
 			print("]\n")
+
+	def print_matrix2(self, mat):
+		# Loop over all rows
+		for i in range(0, len(mat)):
+			print("[", end = "")
+			# Loop over each column in row i
+			for j in range(0, len(mat[i])):
+				# Print out the value in row i, column j
+				print(mat[i][j], end = "")
+				# Only add a tab if we're not in the last column
+				if j != len(mat[i]) - 1:
+					print("  ", end = "")
+			print("]\n")
 	
 	# Utility function that initializes the matrix with all 0's
-	def zeros(self, rows, cols):
-		retval = []
+	def init_matrix(self, rows, cols):
+		matrix = []
 		for x in range(rows):
-			retval.append([])
+			matrix.append([])
 			for y in range(cols):
-				retval[-1].append(0)
-		return retval
+				matrix[-1].append(0)
+		return matrix
 
 
